fb5352744e0815536954d8fe09f6b8a44a2435f4
JohanAbrahamssonHis <118519456+JohanAbrahamssonHis@users.noreply.github.com>
2024-06-11 18:13:15 +0200

    Sun update

--- Assets/Scripts/WaterEffects.cs
+++ Assets/Scripts/WaterEffects.cs
@@ -1,25 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
+using UnityEngine.Rendering;
 using UnityEngine.Rendering.PostProcessing;
+using UnityEngine.Rendering.RendererUtils;
+using RenderSettings = UnityEngine.RenderSettings;
 
 public class WaterEffects : MonoBehaviour
 {
+    public Light sun;
+    
     public Color underwaterFogColor = new Color(0.1f, 0.4f, 0.6f, 1.0f);
     public float underwaterFogStart = 4;
     public float underwaterFogEnd = 35;
     public PostProcessProfile underwaterPostProcessProfile;
+    public Color underwaterSunColor;
     
     private Color originalFogColor;
     private float originalFogStart = 15;
     private float originalFogEnd = 100;
     private PostProcessProfile originalPostProcessProfile;
+    private Color orignialSunColor;
     
     void Start()
     {
         originalFogColor = RenderSettings.fogColor;
         originalFogStart = RenderSettings.fogStartDistance;
         originalFogEnd = RenderSettings.fogEndDistance;
+        orignialSunColor = sun.color;
     }
 
     public void WaterEffectActive()
@@ -27,6 +36,8 @@ public class WaterEffects : MonoBehaviour
         RenderSettings.fogColor = underwaterFogColor;
         RenderSettings.fogStartDistance = underwaterFogStart;
         RenderSettings.fogEndDistance = underwaterFogEnd;
+        RenderSettings.ambientMode = AmbientMode.Trilight;
+        sun.color = underwaterSunColor;
         if (underwaterPostProcessProfile != null)
         {
             var postProcessVolume = Camera.main.GetComponent<PostProcessVolume>();
@@ -43,6 +54,8 @@ public class WaterEffects : MonoBehaviour
         RenderSettings.fogColor = originalFogColor;
         RenderSettings.fogStartDistance = originalFogStart;
         RenderSettings.fogEndDistance = originalFogEnd;
+        RenderSettings.ambientMode = AmbientMode.Skybox;
+        sun.color = orignialSunColor;
         if (underwaterPostProcessProfile != null)
         {
             var postProcessVolume = Camera.main.GetComponent<PostProcessVolume>();
