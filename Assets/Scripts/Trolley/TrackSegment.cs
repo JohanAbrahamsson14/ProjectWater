b6f12eb597fd59536d9aed846721249aec0b3145
JohanAbrahamssonHis <118519456+JohanAbrahamssonHis@users.noreply.github.com>
2024-07-29 18:11:28 +0200

    mini thing

---
+++ Assets/Scripts/Trolley/TrackSegment.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class TrackSegment : MonoBehaviour
+{
+    public List<Transform> keyPoints = new List<Transform>();
+    public List<Transform> controlPoints = new List<Transform>();
+}
